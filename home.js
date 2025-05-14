bf36a8da2d0e0036b9c7687ffb77e557214c20a5
Asit-Kumar-Raut <asitraut2006@gmail.com>
2025-05-14 19:20:15 +0530

    Update home.js

--- home.js
+++ home.js
@@ -609,3 +609,16 @@ document.addEventListener('DOMContentLoaded', () => {
         initVisitorTracking();
     }
 });
+
+window.addEventListener('scroll', () => {
+  const reveals = document.querySelectorAll('.reveal');
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const revealTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+    
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    }
+  }
+});
